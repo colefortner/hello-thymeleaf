1fdd34664950bb360525bc4eddb7bfc44517e896
Cole Fortner <colefortner@gmail.com>
2021-09-13 22:45:43 -0400

    Initial commit

---
+++ src/test/java/org/launchcode/hellothymeleaf/HelloThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.hellothymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
